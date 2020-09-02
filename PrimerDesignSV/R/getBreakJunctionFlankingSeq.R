98f892347ad9229db5c3c9cfd80ed8417d0d5504
Borisklwong <44147120+Borisklwong@users.noreply.github.com>
2020-09-02 11:25:35 +1000

    adding names in the getBreakpointFlankingSeq result

--- PrimerDesignSV/R/getBreakJunctionFlankingSeq.R
+++ PrimerDesignSV/R/getBreakJunctionFlankingSeq.R
@@ -1,9 +1,23 @@
 #'
 #' @export
 getBreakJunctionFlankingSeq = function (gr, bsgenome=BSgenome.Hsapiens.UCSC.hg19, flank.length=250) {
-  ifelse(
-    as.logical(strand(gr)=="+"),
-    Biostrings::getSeq(bsgenome,names = GenomeInfoDb::seqnames(gr), start=BiocGenerics::start(gr)-flank.length+1, width = flank.length),
-    Biostrings::reverseComplement(Biostrings::getSeq(bsgenome,names = GenomeInfoDb::seqnames(gr), start=BiocGenerics::start(gr), width = flank.length))
-  )
+  left_flank_gr = GRanges(
+    seqnames = GenomeInfoDb::seqnames(gr),
+    ranges = IRanges(start=BiocGenerics::start(gr)-flank.length+1, width = flank.length),
+    strand = "+",
+    seqinfo = seqinfo(bsgenome))
+  left_flank_gr = trim(left_flank_gr)
+  left_flank = Biostrings::getSeq(bsgenome,left_flank_gr)
+
+  right_flank_gr = GRanges(
+    seqnames = GenomeInfoDb::seqnames(gr),
+    ranges = IRanges(start=BiocGenerics::start(gr), width = flank.length),
+    strand = "-",
+    seqinfo = seqinfo(bsgenome))
+  right_flank_gr = trim(right_flank_gr)
+  right_flank = Biostrings::getSeq(bsgenome,right_flank_gr)
+  seq = ifelse(as.logical(strand(gr)=="+"),left_flank, right_flank)
+  return(DNAStringSet(seq))
 }
+
+
--- PrimerDesignSV/R/getBreakpointFlankingSeq.R
+++ PrimerDesignSV/R/getBreakpointFlankingSeq.R
@@ -1,14 +1,22 @@
 #' @export
-getBreakpointFlankingSeq=function(gr, bsgenome = BSgenome.Hsapiens.UCSC.hg19, flank.length = 250) {
+getBreakpointFlankingSeq=function(gr, bsgenome = BSgenome.Hsapiens.UCSC.hg19,
+                                  flank.length = 250, export_fasta=TRUE,
+                                  fasta_file_name="BreakpointFlankingSeq.fasta")
+  {
+
   if (is.null(gr$insSeq)) {
     gr$insSeq = ""
   }
-  if ("try-error" %in% class(try(paste0(getBreakJunctionFlankingSeq(gr, bsgenome, flank.length), gr$insSeq, spgs::reverseComplement(getBreakJunctionFlankingSeq(StructuralVariantAnnotation::partner(gr), bsgenome, flank.length), case = "upper"))))) {
-    print("NNNNNNNNNNNNNNNNNNNN")
-
-  } else {
-  paste0(getBreakJunctionFlankingSeq(gr, bsgenome, flank.length), gr$insSeq, spgs::reverseComplement(getBreakJunctionFlankingSeq(StructuralVariantAnnotation::partner(gr), bsgenome, flank.length), case = "upper"))
-  }
+  local = getBreakJunctionFlankingSeq(gr, bsgenome, flank.length)
+  insert = gr$insSeq
+  remote = Biostrings::reverseComplement(getBreakJunctionFlankingSeq(StructuralVariantAnnotation::partner(gr), bsgenome, flank.length))
+  #adding names
+  BreakpointFlankingSeq=DNAStringSet(paste0(local, insert, remote))
+  names(BreakpointFlankingSeq) = names(gr)
+  return(BreakpointFlankingSeq)
+  #Writing output as fasta
+  if (export_fasta==TRUE) {
+    writeXStringSet(BreakpointFlankingSeq, fasta_file_name)
+  } else {print("export_fasta is off")}
 }
-
 
